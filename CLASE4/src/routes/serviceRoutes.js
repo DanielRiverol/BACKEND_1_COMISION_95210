5852ef162c98aaf8fb8eabb10bc1409cc92a3dcb
Eric Benitez <99042388+EricBz@users.noreply.github.com>
2026-08-04 00:15:19 -0300

    Actualizado

--- CLASE4/src/routes/serviceRoutes.js
+++ CLASE4/src/routes/serviceRoutes.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { mostrarTodos } from "../controllers/serviceControllers.js";
+import { actualizar, borrar, crear, mostrarTodos, motrarPorId } from "../controllers/serviceControllers.js";
 
 const serviceRoutes = Router();
 
 serviceRoutes.get("/", mostrarTodos);
+
+serviceRoutes.get("/:id", motrarPorId);
+
+serviceRoutes.post("/", crear);
+
+serviceRoutes.put("/:id", actualizar);
+
+serviceRoutes.delete("/:id", borrar);
 
 export default serviceRoutes
