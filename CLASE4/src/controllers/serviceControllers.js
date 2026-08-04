2308367eb237c4bd853567ea15c2d578e46e8ac7
Eric Benitez <99042388+EricBz@users.noreply.github.com>
2026-08-04 00:14:30 -0300

    Actualizado

--- CLASE4/src/controllers/serviceControllers.js
+++ CLASE4/src/controllers/serviceControllers.js
@@ -9,3 +9,44 @@ export const mostrarTodos = async (req, res) => {
     }
     res.status(200).json(mostrar);
 }
+
+export const motrarPorId = async (req, res) => {
+    const id = req.params.id
+    console.log(id)
+    const mostrar = await service.buscarPorId(id)
+    if (!mostrar) {
+        res.status(404).json({message:"Servicio no encontrado..."})
+    }
+    res.status(200).json(mostrar)
+}
+
+export const crear = async (req, res) => {
+    const nuevoServicio = req.body;
+    const mostrar = await service.crear(nuevoServicio)
+    console.log(mostrar)
+    res.status(200).json(mostrar)
+
+}
+
+export const actualizar = async (req, res) => {
+    const id = req.params.id;
+    const serviceUpdate = req.body;
+    if (!id || !serviceUpdate) {
+        res.status(400).json({message:"Faltan datos..."})
+    }
+    const mostrar = await service.actualizar(id, serviceUpdate);
+    res.status(200).json({mostrar})
+}
+
+export const borrar = async (req, res) => {
+    const id = req.params.id;
+    if (!id) {
+        res.status(400).json({message:"Por favor ingrese un id."})
+    }
+    const search = await service.buscarPorId(id);
+    if (!search) {
+        res.status(400).json({message: "No se encuentra el usuario."})
+    }
+    await service.borrar(id);
+    res.status(200).json(await service.obtenerTodos())
+}
